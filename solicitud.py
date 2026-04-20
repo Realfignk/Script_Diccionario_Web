44655aaa3080897b6da078b25dc5f62731f44e20
Realfignk <realfignk@gmail.com>
2026-04-20 16:00:35 -0600

    Update solicitud.py

--- solicitud.py
+++ solicitud.py
@@ -2,20 +2,24 @@ import requests
 
 lista_palabras="diccionario.txt"
 
-url=input("Ingresa la url (con puerto si aplica): ")
+url=input("Ingresa la url (con puerto si no es ek por defecto [80]): ")
 
+print("""
+------------------------------------------------
+Iniciando búsqueda de archivos en la url...
+------------------------------------------------
+""")
 try:
     with open("diccionario.txt", 'r', encoding='utf') as archivo:
         for linea in archivo:
             r=requests.get(url.rstrip('\n') + "/" + linea.rstrip('\n'))
             if r.status_code < 400:
                 print(f"    Existe {url}/{linea}")
-except FileNotFoundError:
-    print("No se puede abrir el diccionario.")
 
-#print(r)
-#print(dir(r))
-#print(help(r))
-#print(r.text)
-#print(r.status_code)
-#print(r.headers)
+    print("""
+    ------------------------------------------------
+    Proceso de búsqueda de archivos finalizado.
+    ------------------------------------------------
+    """)
+except FileNotFoundError:
+    print("No se puede abrir el diccionario, verifique que exista.")
