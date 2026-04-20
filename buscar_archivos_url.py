17e7ef1a1a40e1ba03ef64d8ab5702091c97aa49
Realfignk <realfignk@gmail.com>
2026-04-20 16:21:40 -0600

    Update buscar_archivos_url.py

--- buscar_archivos_url.py
+++ buscar_archivos_url.py
@@ -9,13 +9,16 @@ print("""
 Iniciando búsqueda de archivos en la url...
 ------------------------------------------------
 """)
-try:
-    with open("diccionario.txt", 'r', encoding='utf') as archivo:
-        for linea in archivo:
-            r=requests.get(url.rstrip('\n') + "/" + linea.rstrip('\n'))
-            if r.status_code < 400:
-                print(f"    Existe {url}/{linea}")
 
+try:
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    with open("diccionario.txt", 'r', encoding='utf') as archivo:
+        for linea in archivo:       
+            respuesta = requests.get(url.rstrip('\n') + "/" + linea.rstrip('\n'), headers=headers)
+            if respuesta.status_code == 200:
+                print(f"    Existe {url}/{linea}")
+            elif respuesta.status_code == 403:
+                print(f"    Prohibido {url}/{linea}")
     print("""
     ------------------------------------------------
     Proceso de búsqueda de archivos finalizado.
@@ -23,3 +26,5 @@ try:
     """)
 except FileNotFoundError:
     print("No se puede abrir el diccionario, verifique que exista.")
+except requests.exceptions.RequestException as e:
+        print(f"Error al conectar con la página: {e}")
